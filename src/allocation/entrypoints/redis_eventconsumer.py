05bd040cc115e559a1b6bb1cb5181872a1773042
jwdinius <josephwdinius@gmail.com>
2022-08-20 08:52:17 -0700

    (jwd) ch11 tests passing

--- src/allocation/entrypoints/redis_eventconsumer.py
+++ src/allocation/entrypoints/redis_eventconsumer.py
@@ -15,16 +15,29 @@ r = redis.Redis(**config.get_redis_host_and_port())
 def main():
     orm.start_mappers()
     pubsub = r.pubsub(ignore_subscribe_messages=True)
+    #pubsub = r.pubsub(ignore_subscribe_messages=False)
     pubsub.subscribe("change_batch_quantity")
+    pubsub.subscribe("allocation")
 
     for m in pubsub.listen():
-        handle_change_batch_quantity(m)
+        logging.warning("## {}".format(m))
+        channel = m["channel"].decode('UTF-8')
+        if channel == "change_batch_quantity":
+            handle_change_batch_quantity(m)
+        elif channel == "allocation":
+            handle_allocation(m)
 
 
 def handle_change_batch_quantity(m):
     logging.debug("handling %s", m)
     data = json.loads(m["data"])
     cmd = commands.ChangeBatchQuantity(ref=data["batchref"], qty=data["qty"])
+    messagebus.handle(cmd, uow=unit_of_work.SqlAlchemyUnitOfWork())
+
+def handle_allocation(m):
+    logging.debug("handling %s", m)
+    data = json.loads(m["data"])
+    cmd = commands.Allocate(orderid=data["orderid"], sku=data["sku"], qty=data["qty"])
     messagebus.handle(cmd, uow=unit_of_work.SqlAlchemyUnitOfWork())
 
 
--- tests/e2e/test_external_events.py
+++ tests/e2e/test_external_events.py
@@ -36,3 +36,34 @@ def test_change_batch_quantity_leading_to_reallocation():
             data = json.loads(messages[-1]["data"])
             assert data["orderid"] == orderid
             assert data["batchref"] == later_batch
+
+
+@pytest.mark.usefixtures("postgres_db")
+@pytest.mark.usefixtures("restart_api")
+@pytest.mark.usefixtures("restart_redis_pubsub")
+def test_allocation():
+    # start with two batches
+    orderid, sku = random_orderid(), random_sku()
+    earlier_batch, later_batch = random_batchref("old"), random_batchref("newer")
+    api_client.post_to_add_batch(earlier_batch, sku, qty=10, eta="2011-01-01")
+    api_client.post_to_add_batch(later_batch, sku, qty=10, eta="2011-01-02")
+
+    # allocate order
+    redis_client.publish_message(
+        "allocation",
+        {"orderid": orderid, "sku": sku, "qty": 5},
+    )
+    
+    subscription = redis_client.subscribe_to("line_allocated")
+
+    # wait until we see a message saying the order has been allocated (to earlier_batch)
+    messages = []
+    for attempt in Retrying(stop=stop_after_delay(3), reraise=True):
+        with attempt:
+            message = subscription.get_message(timeout=1)
+            if message:
+                messages.append(message)
+                print(messages)
+            data = json.loads(messages[-1]["data"])
+            assert data["orderid"] == orderid
+            assert data["batchref"] == earlier_batch
